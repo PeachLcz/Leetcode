26eafb61a1dd1bc028366369f11259ca87f51ae8
PeachLcz <43182871+PeachLcz@users.noreply.github.com>
2018-09-11 15:05:37 -0700

    Update 1 Two Sum.java

--- Java/1 Two Sum.java
+++ Java/1 Two Sum.java
@@ -17,10 +17,10 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target)
-                    return new int[]{i,j};
+                    return new int[]{i,j};     //pay attention to the syntax 
             }
         }
-        throw new IllegalArgumentException("No two sum solution");
+        throw new IllegalArgumentException("No two sum solution"); //pay attention to the syntax
     }
 }
 
