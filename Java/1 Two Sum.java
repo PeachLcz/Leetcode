5213c8fece098f210f3909958c0f3cf2a88c4baa
PeachLcz <43182871+PeachLcz@users.noreply.github.com>
2018-09-11 14:55:27 -0700

    Update 1 Two Sum.java

--- Java/1 Two Sum.java
+++ Java/1 Two Sum.java
@@ -10,6 +10,8 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+- Approach1: Brute force
+- Time Complexity:O(n^2)    Space Complexity:O(1)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length-1;i++){
@@ -21,3 +23,40 @@ class Solution {
         throw new IllegalArgumentException("No two sum solution");
     }
 }
+
+-Approach2: HashMap-- Trade space for speed, traverse the array twice
+-Time Complexity:O(n)    Space Complexity:O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            int newTarget=target-nums[i];
+            if(map.containsKey(newTarget)&&map.get(newTarget)!=i) 
+                return new int[]{i,map.get(newTarget)};
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+-Approach3: Improvement of Approach2: traverse the array only once
+-Time Complexity:O(n)    Space Complexity:O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int newTarget=target-nums[i];
+            if(map.containsKey(newTarget)) 
+                return new int[]{i,map.get(newTarget)};
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+
+
