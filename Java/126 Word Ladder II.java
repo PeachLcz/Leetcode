a5b0513a5887c6c9556c0f11187daaf5b7292429
PeachLcz <43182871+PeachLcz@users.noreply.github.com>
2018-09-18 21:02:40 -0700

    Update 126 Word Ladder II.java

--- Java/126 Word Ladder II.java
+++ Java/126 Word Ladder II.java
@@ -4,35 +4,39 @@ class Solution {
     List<List<String>> res=new ArrayList();
     HashMap<String,List<String>> map=new HashMap();
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        if(wordList==null||wordList.size()==0) return res;
-        HashSet<String> dic=new HashSet(wordList);
-        HashSet<String> start=new HashSet();
-        HashSet<String> end=new HashSet();
-        if(!dic.contains(endWord)) return res;
-        start.add(beginWord);
-        end.add(endWord);
-        buildMap(start,end,dic,false);
+        if(wordList==null) return res;
+        Set<String> dict=new HashSet(wordList);
+        Set<String> beginSet=new HashSet();
+        Set<String> endSet=new HashSet();
+        if(!dict.contains(endWord)) return res;
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        buildMap(beginSet,endSet,dict,false);
         List<String> path=new ArrayList();
         path.add(beginWord);
         dfs(beginWord,endWord,path);
         return res;
     }
-    public void buildMap(HashSet<String> start,HashSet<String> end,HashSet<String> dic,boolean reverse){
-        if(start.size()==0) return;
-        if(start.size()>end.size()){buildMap(end,start,dic,!reverse);return;}
-        HashSet<String> next=new HashSet();
-        dic.removeAll(start);
+    private void buildMap(Set<String> beginSet,Set<String> endSet,Set<String> dict,boolean reverse){
+        if(beginSet.size()==0) return;
+        if(beginSet.size()>endSet.size()){
+            buildMap(endSet,beginSet,dict,!reverse);
+            return;
+        }
         boolean finished=false;
-        for(String s:start){
+        Set<String> next=new HashSet();
+        dict.removeAll(beginSet);
+        for(String s:beginSet){
             char[] arr=s.toCharArray();
             for(int i=0;i<arr.length;i++){
-                char tmp=arr[i];
-                for(char c='a';c<='z';c++){
-                    if(c==tmp) continue;
-                    arr[i]=c;
+                char temp=arr[i];
+                for(char ch='a';ch<='z';ch++){
+                    if(ch==temp) continue;
+                    arr[i]=ch;
                     String nStr=new String(arr);
-                    if(dic.contains(nStr)){
-                        if(end.contains(nStr)) finished=true;
+                    if(dict.contains(nStr)){
+                        if(endSet.contains(nStr))
+                            finished=true;
                         else next.add(nStr);
                         String key=reverse?nStr:s;
                         String val=reverse?s:nStr;
@@ -40,20 +44,21 @@ class Solution {
                         map.get(key).add(val);
                     }
                 }
-                arr[i]=tmp;
+                arr[i]=temp;
             }
         }
-        if(!finished) buildMap(next,end,dic,reverse);
+        if(!finished) buildMap(next,endSet,dict,reverse);
     }
-    public void dfs(String begin,String end,List<String> path){
-        if(begin.equals(end)){
+    private void dfs(String beginWord,String endWord,List<String> path){
+        if(beginWord.equals(endWord)){
             res.add(new ArrayList(path));
             return;
         }
-        if(!map.containsKey(begin)) return;
-        for(String next:map.get(begin)){
+        if(!map.containsKey(beginWord))
+            return;
+        for(String next:map.get(beginWord)){
             path.add(next);
-            dfs(next,end,path);
+            dfs(next,endWord,path);
             path.remove(path.size()-1);
         }
     }
